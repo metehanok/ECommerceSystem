e3eb58261ae93979df67c4d7b74f5038b36d8e2a
Metehan Ok <metet.ok@outlook.com>
2025-05-08 21:15:27 +0300

    Azure Connection Created, Ready For Deploying V4

--- ECommerceSystem.Data/obj/Debug/net8.0/ECommerceSystem.Data.AssemblyInfo.cs
+++ ECommerceSystem.Data/obj/Debug/net8.0/ECommerceSystem.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerceSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7abc98ec429252caddbd48b2a22a9aeabbd3b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb7e3f28df00b033b8b2b082932d62f2955eb7bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerceSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerceSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
